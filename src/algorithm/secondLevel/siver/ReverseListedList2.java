9a87aa05d7606830f72f8867b8b6adebed1b8b76
sunyuhai <1099620895@qq.com>
2023-08-31 13:11:26 +0800

    反转链表2

--- src/algorithm/secondLevel/siver/ReverseListedList2.java
+++ src/algorithm/secondLevel/siver/ReverseListedList2.java
@@ -33,21 +33,26 @@ public class ReverseListedList2 {
     public static ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode newhead = new ListNode(-1);
         newhead.next = head;
-        ListNode index1 = newhead;
-        ListNode index2 = newhead;
+        //输入：head = [-1,1,2,3,4,5], left = 2, right = 4
+        //输出：[1,4,3,2,5]
+        ListNode leftNode = newhead;
+        ListNode rigthNode = newhead;
         for (int i = 0; i < left - 1; i++) {
-            index1 = index1.next;
+            leftNode = leftNode.next;
         }
+        ListNode reverseNode = leftNode.next;
         for (int i = 0; i < right; i++) {
-            index2 = index2.next;
+            rigthNode = rigthNode.next;
         }
-        ListNode midNode = index1.next;
-        ListNode foot = index2.next;
-        index1.next = null;
-        index2.next = null;
-        reverseList(midNode);
-        index1.next = index2;
-        midNode.next = foot;
+        ListNode foot = rigthNode.next;
+
+        rigthNode.next = null;
+        leftNode.next = null;
+        reverseList(reverseNode);
+
+        leftNode.next = rigthNode;
+        reverseNode.next = foot;
+
         return newhead.next;
     }
 
