31a57193cbadceda124c77adf5bd89600359744d
sunyuhai <1099620895@qq.com>
2023-09-16 21:34:57 +0800

    搜索插入位置

---
+++ src/algorithm/thirdLevel/bronze/SearchInsert.java
@@ -0,0 +1,34 @@
+package algorithm.thirdLevel.bronze;
+
+/**
+ * 题目：搜索插入位置
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ */
+public class SearchInsert {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,5,6};
+        System.out.println(searchInsert(arr, 2));
+    }
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int rigth = nums.length - 1;
+        int mid = 0;
+        while (left <= rigth){
+            mid = left + (rigth - left) / 2;
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else if(nums[mid] > target){
+                rigth = mid - 1;
+            }else {
+                return mid;
+            }
+        }
+        if (nums[mid] > target){
+            return mid;
+        }else {
+            return mid + 1;
+        }
+    }
+}
