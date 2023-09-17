16470d2f825428194eba0ff897f20191b0256034
sunyuhai <1099620895@qq.com>
2023-09-17 15:23:02 +0800

    颜色分类

---
+++ src/algorithm/thirdLevel/gold/SortColors.java
@@ -0,0 +1,40 @@
+package algorithm.thirdLevel.gold;
+
+import algorithm.thirdLevel.bronze.TraversalArray;
+
+/**
+ * 题目：颜色分类
+ * 给定一个包含红色、白色和蓝色、共 n 个元素的数组nums ，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ * 我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+ * 必须在不使用库内置的 sort 函数的情况下解决这个问题。
+ * 输入：nums = [2,0,2,1,1,0]
+ * 输出：[0,0,1,1,2,2]
+ */
+public class SortColors {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,0,2,1,1,0};
+        sortColors(arr);
+        TraversalArray.arrayPrint(arr);
+    }
+
+    public static void sortColors(int[] nums) {
+        int left = 0;
+        int rigth = nums.length - 1;
+        int index = 0;
+        while (index <= rigth){
+            if (nums[index] == 0){
+                swap(nums,index++,left++);
+            }else if (nums[index] == 2){
+                swap(nums,index,rigth--);
+            }else {
+                index++;
+            }
+        }
+    }
+
+    public static void swap(int[] nums,int i,int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
