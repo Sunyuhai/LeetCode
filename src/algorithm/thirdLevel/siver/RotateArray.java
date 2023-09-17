1d74a40e57bf42cdd8b58cc98fe8652c4b54d1f7
sunyuhai <1099620895@qq.com>
2023-09-17 10:32:50 +0800

    轮转数组

---
+++ src/algorithm/thirdLevel/siver/RotateArray.java
@@ -0,0 +1,38 @@
+package algorithm.thirdLevel.siver;
+
+import algorithm.thirdLevel.bronze.TraversalArray;
+
+/**
+ * 题目：轮转数组
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右轮转 1 步: [7,1,2,3,4,5,6]
+ * 向右轮转 2 步: [6,7,1,2,3,4,5]
+ * 向右轮转 3 步: [5,6,7,1,2,3,4]
+ */
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,6,7};
+        rotate(arr,3);
+        TraversalArray.arrayprint(arr);
+    }
+
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] arr,int start,int end){
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
--- src/kylin/Solution.java
+++ src/kylin/Solution.java
@@ -5,28 +5,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("abcazxcvn"));
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
-
-    public static int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() <= 0) {
-            return 0;
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
         }
-
-        int leftIndex = -1;
-        int result = 0;
-        Map<Character, Integer> leftIndexMap = new HashMap<Character, Integer>();
-        char[] charArray = s.toCharArray();
-        for (int i = 0; i < s.length(); i++) {
-            if (leftIndexMap.containsKey(charArray[i])) {   // 当前数字 在之前遍历时出现过，就更新 窗口左边界
-                leftIndex = Math.max(leftIndex, leftIndexMap.get(charArray[i]));
-            }
-            leftIndexMap.put(charArray[i], i);
-
-            result = Math.max(result, i - leftIndex);   // 计算 当前结果
-        }
-        return result;
     }
 }
-
