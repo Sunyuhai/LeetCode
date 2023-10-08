eba603a9a9ae2e2c44239e21eb3bc6bb012358b1
sunyuhai <1099620895@qq.com>
2023-10-08 18:35:36 +0800

    二叉树的前序遍历

---
+++ src/algorithm/seventhLevel/siver/PreorderTraversal.java
@@ -0,0 +1,38 @@
+package algorithm.seventhLevel.siver;
+
+
+import algorithm.seventhLevel.siver.tree.StructureBinaryTree;
+import algorithm.seventhLevel.siver.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 题目：二叉树的前序遍历
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ * 示例 1：
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,2,3]
+ */
+public class PreorderTraversal {
+    public static void main(String[] args) {
+        TreeNode root = StructureBinaryTree.buildTree(new int[]{1, -1, 2, 3});
+        List<Integer> result = preorderTraversal(root);
+        for (int em : result) {
+            System.out.print(em + " ");
+        }
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, result);
+        return result;
+    }
+
+    public static void dfs(TreeNode head, List<Integer> result) {
+        if (head == null) return;
+        result.add(head.val);
+        dfs(head.left, result);
+        dfs(head.right, result);
+    }
+}
