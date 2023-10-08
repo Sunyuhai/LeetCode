41f11985fe5dcc2ac9758d8c3ea701a0a5bb7900
sunyuhai <1099620895@qq.com>
2023-10-08 18:31:50 +0800

    构建二叉树

--- src/algorithm/seventhLevel/siver/tree/TreeNode.java
+++ src/algorithm/seventhLevel/siver/tree/TreeNode.java
@@ -2,8 +2,8 @@ package algorithm.seventhLevel.siver.tree;
 
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
 
     public TreeNode(int val) {
         this.val = val;
