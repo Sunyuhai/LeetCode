a055c1f6dfd2f0ca3f90896f20eaddf305d5ee3c
sunyuhai <1099620895@qq.com>
2024-05-07 11:06:28 +0800

    fix

--- src/kylin/demo.java
+++ src/kylin/demo.java
@@ -2,13 +2,23 @@ package kylin;
 
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class demo {
     public static void main(String[] args) {
-        String str = "PB,AM,00";
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        map.put(1, 6);
+        map.put(2, 7);
+        map.put(3, 5);
+        map.put(4, 0);
+        map.put(5, 8);
+        map.put(6, 1);
+        map.put(7, 0);
+
+        map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEach(
+                e -> System.out.println(e.getKey())
+        );
+
     }
 
 
