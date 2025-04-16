6331793bfa4c015842087e3b15cd394366cef47f
sunyuhai <1099620895@qq.com>
2025-04-16 16:56:11 +0800

    fix

--- src/kylin/demo.java
+++ src/kylin/demo.java
@@ -6,13 +6,26 @@ import java.util.stream.Collectors;
 
 public class demo {
     public static void main(String[] args) {
-        String ss = "abba";
-
-        System.out.printf("ss", lengthOfLongestSubstring(ss));
-
+        String s = "pwwkew";
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left++;
+                map.remove(s.charAt(i));
+            }
+            right++;
+            map.put(s.charAt(i), i);
+            max = Math.max(max, right - left);
+        }
+        System.out.println(max);
     }
 
-    public static int lengthOfLongestSubstring(String s) {
+
+
+    public int lengthOfLongestSubstring(String s) {
         if(s.length() == 0) return 0;
         // abcabcbb
         int i = 0;
@@ -20,7 +33,8 @@ public class demo {
         HashMap<Character,Integer> map = new HashMap();
         for (int j = 0; j < s.length(); j++) {
             if (map.containsKey(s.charAt(j))){
-                i = Math.max(i,map.get(s.charAt(j)) + 1);
+//                i = Math.max(i,map.get(s.charAt(j)) + 1);
+                i = map.get(s.charAt(j)) + 1;
             }
             map.put(s.charAt(j),j);
             maxLen = Math.max(maxLen,j - i + 1);
